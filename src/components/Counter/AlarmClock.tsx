4472ff8c462c4b0480c8da7d22f9c161fa7c562c
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2024-03-04 16:45:35 +0000

    added input

--- src/components/Counter/AlarmClock.tsx
+++ src/components/Counter/AlarmClock.tsx
@@ -15,41 +15,48 @@ import {
     tap,
 } from "rxjs";
 
-const countdown$: Observable<number> = interval(250).pipe(
-    startWith(10),
-    scan((time) => time - 1),
-    takeWhile((time) => time > 0)
-);
-
 const action$: Subject<string> = new Subject<string>();
-const snooze$: Observable<string> = action$
-    .pipe(filter((action) => action === "snooze"))
-    .pipe(tap((ev) => console.log(ev)));
-const dismiss$ = action$.pipe(filter((action) => action === "dismiss"));
-
-const snoozeAlarm$: Observable<number | string> = concat(
-    countdown$,
-    of("Wake up!")
-).pipe(repeat({ delay: () => snooze$ }));
-
-const observable$: Observable<number | string> = concat(
-    snoozeAlarm$.pipe(takeUntil(dismiss$)),
-    of("Have a nice day!")
-);
 
 const AlarmClock: React.FC = () => {
     const [time, setTime] = useState<number | string>();
+    const [startValue, setStartValue] = useState<number>(10);
+
     useEffect(() => {
+        const countdown$: Observable<number> = interval(250).pipe(
+            startWith(startValue),
+            scan((time) => time - 1),
+            takeWhile((time) => time > 0)
+        );
+
+        const snooze$: Observable<string> = action$
+            .pipe(filter((action) => action === "snooze"))
+            .pipe(tap((ev) => console.log(ev)));
+        const dismiss$ = action$.pipe(filter((action) => action === "dismiss"));
+
+        const snoozeAlarm$: Observable<number | string> = concat(
+            countdown$,
+            of("Wake up!")
+        ).pipe(repeat({ delay: () => snooze$ }));
+
+        const observable$: Observable<number | string> = concat(
+            snoozeAlarm$.pipe(takeUntil(dismiss$)),
+            of("Have a nice day!")
+        );
         const subscribtion: Subscription = observable$.subscribe({
             next: (val) => setTime(val),
             error: (err) => console.log(err),
             complete: () => console.log("completed"),
         });
         return () => subscribtion.unsubscribe();
-    }, []);
+    }, [startValue]);
     return (
         <>
             <p>{time}</p>
+            <input
+                type="number"
+                onChange={(e) => setStartValue(parseInt(e.target.value) || 0)}
+                placeholder="Enter start time"
+            />
             <button id="snoozeBtn" onClick={() => action$.next("snooze")}>
                 Snooze
             </button>
